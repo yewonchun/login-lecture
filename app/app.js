161d9d36c803e262feed3d8f4beee252abc8b1d6
yewon <yewon@MacRentui-MacBookPro.local>
2021-07-19 00:58:57 +0900

    front단 js 파일 만들기

--- app/app.js
+++ app/app.js
@@ -5,11 +5,13 @@ const express = require("express");
 const app = express();
 
 // 라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
+
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
 
